3c8549d5ae809880ff4648140b39006991fe7b4d
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2019-12-09 23:53:53 +0100

    refactor: Cambiar algoritmo y nombre func full
    He cambiado el algoritmo y el nombre de la función full para una mejor
    lectura de la misma

--- yatzy.py
+++ yatzy.py
@@ -161,34 +161,14 @@ class Yatzy:
             return 20
 
     @staticmethod
-    def fullHouse( d1,  d2,  d3,  d4,  d5):
-        tallies = []
-        _2 = False
-        i = 0
-        _2_at = 0
-        _3 = False
-        _3_at = 0
-
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-
-        for i in range(6):
-            if (tallies[i] == 2): 
-                _2 = True
-                _2_at = i+1
-            
-
-        for i in range(6):
-            if (tallies[i] == 3): 
-                _3 = True
-                _3_at = i+1
-            
-
-        if (_2 and _3):
-            return _2_at * 2 + _3_at * 3
-        else:
-            return 0
+    def full_house(*dice):
+        list_numbers = []
+        for die in dice:
+            if dice.count(die) == 2 or dice.count(die) == 3:
+                if list_numbers.count(die) == 0:
+                    list_numbers.append(die)
+                else:
+                    continue
+            else:
+                return 0
+        return (list_numbers[0] * dice.count(list_numbers[0])) + (list_numbers[1] * dice.count(list_numbers[1]))
