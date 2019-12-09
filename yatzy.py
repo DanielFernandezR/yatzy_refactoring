f5f5c1b42fc668e538744bf6a6ba6cbe5c348067
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2019-12-09 23:02:21 +0100

    refactor: Cambiar algoritmo y nombre small straight
    He cambiado el algoritmo y el nombre de la funcion del small straight
    para una mejor lectura del codigo

--- yatzy.py
+++ yatzy.py
@@ -135,21 +135,21 @@ class Yatzy:
             return list_numbers[0] * four_kind
 
     @staticmethod
-    def smallStraight( d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[0] == 1 and
-            tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-            tallies[4] == 1):
+    def sum_small_straight(*dice):
+        small_number = 1
+        list_numbers = []
+        for die in dice:
+            if dice.count(die) == small_number:
+                if list_numbers.count(die) == 0:
+                    list_numbers.append(die)
+                else:
+                    continue
+            else:
+                return 0
+        if list_numbers == []:
+            return 0
+        else:
             return 15
-        return 0
-    
 
     @staticmethod
     def largeStraight( d1,  d2,  d3,  d4,  d5):
